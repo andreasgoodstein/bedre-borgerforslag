2a8f811dfa15decd1d28b93e12a05b646271dbba
Andreas Goodstein <andreasgoodstein@gmail.com>
2018-12-19 11:54:34 +0100

    Add try catch to timer job

--- src/server/timer.js
+++ src/server/timer.js
@@ -2,14 +2,20 @@ const { updateForslag } = require('./logic/updateforslag')
 const { connect } = require('./database/connect')
 
 const runUpdate = async () => {
-    console.log('Running update job')
-    
-    connect()
-    await updateForslag()
+    try {
+        console.log('Running update job')
+        
+        connect()
+        await updateForslag()
 
-    console.log('Finished update')
+        console.log('Finished update')
 
-    process.exit()
+        process.exit()  
+    } 
+    catch (error) {
+        console.error(error)
+        process.exit()
+    }
 }
 
-runUpdate()
+runUpdate()  
