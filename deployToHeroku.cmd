aad430c4e6aadcb5e33a5fbfc1f79bc830ceb0f4
Andreas Goodstein <24245735+andreasgoodstein@users.noreply.github.com>
2022-04-09 12:21:08 +0200

    update dependencies

--- deployToHeroku.cmd
+++ deployToHeroku.cmd
@@ -1,4 +1,5 @@
 # heroku container:login
+docker compose build
 docker tag bedreforslag registry.heroku.com/bedreforslag/web
 docker push registry.heroku.com/bedreforslag/web
 heroku container:release web -a bedreforslag
